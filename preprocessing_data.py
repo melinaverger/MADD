69b0ca2b872f05e6f7918e0d417d633d33160c52
Mélina Verger <melinaverger@ordipro.wifi.desir.lip6.fr>
2022-08-31 16:26:49 +0200

    update

--- preprocessing_data.py
+++ preprocessing_data.py
@@ -1,6 +1,7 @@
 """
 Created on: Fri. 5 Aug. 2022
 Updated on: Tue. 9 Aug. 2022
+Updated on: Wed. 31 Aug. 2022
 Author: Mélina Verger
 """
 
@@ -19,10 +20,6 @@ from encode_features import encoding
 # To scale the data
 from mlxtend.preprocessing import minmax_scaling
 
-# Plotting modules
-import seaborn as sns
-import matplotlib.pyplot as plt
-
 
 
 ## Start the clock
@@ -33,23 +30,23 @@ start_time = time.monotonic()
 
 ## Read the data
 
-studentInfo = pd.read_csv("./data/studentInfo.csv")
-print("----------\nRead 'studentInfo.csv'.", flush=True)
+studentAll = pd.read_csv("./data/studentAll.csv")
+print("----------\nRead 'studentInfo.All'.", flush=True)
 
 
 
 ## Remove missing and noisy values (in the rows)
 
 # Missing values
-studentInfo = studentInfo.dropna(subset=['imd_band']) # from previous studies, we know that there are 1,111 missing values within this feature
+studentAll = studentAll.dropna(subset=['imd_band']) # from previous studies, we know that there are 1,111 (then 1,030) missing values within this feature
 
 # Noise
-studentInfo = studentInfo[studentInfo.final_result != "Withdrawn"]
-studentInfo = studentInfo[studentInfo.final_result != "Distinction"]
+studentAll = studentAll[studentAll.final_result != "Withdrawn"]
+# kept for the second xp: studentInfo = studentInfo[studentInfo.final_result != "Distinction"]
 
 # Noise (duplicates)
-studentInfo = studentInfo.drop_duplicates(subset=["id_student"], keep="first")
-studentInfo = studentInfo.drop(columns=["id_student"])
+studentAll = studentAll.drop_duplicates(subset=["id_student"], keep="first")
+studentAll = studentAll.drop(columns=["id_student"])
 
 print("Missing and noisy values removed.", flush=True)
 
@@ -57,25 +54,25 @@ print("Missing and noisy values removed.", flush=True)
 
 ## Encoding categorical features
 
-studentInfo_num = encoding(studentInfo)
+studentAll_num = encoding(studentAll)
 print("Categorical features encoded.", flush=True)
 
-studentInfo_num_columns = studentInfo_num.columns
-studentInfo_num.to_csv("./data/studentInfo_num.csv", index=False)
-print("Numerical data set saved in 'studentInfo_num.csv' file.", flush=True)
+studentAll_num_columns = studentAll_num.columns
+studentAll_num.to_csv("./data/studentAll_num.csv", index=False)
+print("Numerical data set saved in 'studentAll_num.csv' file.", flush=True)
 
 
 
 ## Scale the data (https://www.kaggle.com/code/rtatman/data-cleaning-challenge-scale-and-normalize-data)
 
-studentInfo_num_scaled = minmax_scaling(studentInfo_num, columns = studentInfo_num_columns, min_val=0, max_val=1)
+studentAll_num_scaled = minmax_scaling(studentAll_num, columns = studentAll_num_columns, min_val=0, max_val=1)
 
 
 
 ## Save dataframe
 
-studentInfo_num_scaled.to_csv("./data/studentInfo_num_scaled.csv", index=False)
-print("Numerical scaled data set saved in 'studentInfo_num_scaled.csv' file.", flush=True)
+studentAll_num_scaled.to_csv("./data/studentAll_num_scaled.csv", index=False)
+print("Numerical scaled data set saved in 'studentAll_num_scaled.csv' file.", flush=True)
 
 
 
